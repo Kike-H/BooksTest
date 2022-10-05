ed016f451449786a094b0c61fdd11808a5af79dc
Manuel E. Hernandez <mehernandezduarte@gmail.com>
2022-10-05 18:50:14 -0500

    Initial Commit

---
+++ BooksTest/BooksTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksTestApp.swift
+//  BooksTest
+//
+//  Created by Kike Hernandez D. on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BooksTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
